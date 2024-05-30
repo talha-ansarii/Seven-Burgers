deb472ac2d474c23325f266505b1693829d97539
Talha Ansari <talhaansari1606@gmail.com>
2024-05-30 19:21:50 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import './App.css'
-import { TextRevealCardPreview } from './pages/test'
 function App() {
 
   return (
     <div className='w-full'>
       <Routes>
         <Route path="/" element={<Home/>} />   
-        <Route path="/test" element={<TextRevealCardPreview/>} />   
 
       </Routes>
     </div>
