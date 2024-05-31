fb8687bcf255430c657cc19230144e330738921a
Talha Ansari <talhaansari1606@gmail.com>
2024-05-31 20:57:48 +0530

    added new header

--- src/components/ui/text-reveal-card.jsx
+++ src/components/ui/text-reveal-card.jsx
@@ -107,7 +107,7 @@ function mouseEnterHandler() {
           <p className=" text-[60px] leading-[60px] md:text-[70px] md:leading-[70px] lg:text-[105px] lg:leading-[105px] py-10 font-migra font-bold text-[#152663] ">
             {text}
           </p>
-          <MemoizedStars />
+          
         </div>
       </div>
     </div>
@@ -134,41 +134,5 @@ export const TextRevealCardDescription = ({
   );
 };
 
-const Stars = () => {
-  const randomMove = () => Math.random() * 4 - 2;
-  const randomOpacity = () => Math.random();
-  const random = () => Math.random();
-  return (
-    <div className="absolute inset-0">
-      {[...Array(80)].map((_, i) => (
-        <motion.span
-          key={`star-${i}`}
-          animate={{
-            top: `calc(${random() * 130}% + ${randomMove()}px)`,
-            left: `calc(${random() * 100}% + ${randomMove()}px)`,
-            opacity: randomOpacity(),
-            scale: [1, 1.2, 0],
-          }}
-          transition={{
-            duration: random() * 10 + 20,
-            repeat: Infinity,
-            ease: "linear",
-          }}
-          style={{
-            position: "absolute",
-            top: `${random() * 100}%`,
-            left: `${random() * 100}%`,
-            width: `2px`,
-            height: `2px`,
-            backgroundColor: "white",
-            borderRadius: "50%",
-            zIndex: 1,
-          }}
-          className="inline-block"
-        ></motion.span>
-      ))}
-    </div>
-  );
-};
 
-export const MemoizedStars = memo(Stars);
+
