a8ec56af3d88dd9661d2f2959d13397579e1f226
Talha Ansari <talhaansari1606@gmail.com>
2024-06-15 02:27:46 +0530

    added blog list page

--- src/components/BLogCard.jsx
+++ src/components/BLogCard.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 
-const BLogCard = () => {
+const BlogCard = () => {
   return (
-    <div className="bg-black text-white p-6 rounded-lg shadow-md max-w-md mx-auto">
-      <img src="/path-to-your-image/image.png" alt="Dashboard" className="w-full rounded-lg mb-4" />
-      <div className="text-sm font-semibold text-gray-400 mb-2">PRODUCT UPDATES</div>
-      <h2 className="text-2xl font-bold mb-4">Dashboard refinements and a free plan for Comments</h2>
-      <div className="flex items-center">
-        <img 
-          src="https://via.placeholder.com/40" 
-          alt="User Avatar" 
-          className="w-10 h-10 rounded-full mr-2" 
-        />
-        <div className="text-sm">January 12th, 2024</div>
+    <div className="max-w-sm rounded-lg overflow-hidden cursor-pointer p-4">
+      <img className="w-full  lg:h-48 object-cover rounded-t-[21px]" src="/blogCard.jpg" alt="Burger" />
+      <div className="pt-4">
+        <h2 className="text-lg inter font-[600] text-[#1F4386]">title title lorem ipsum lirem</h2>
+        <div className="flex items-center justify-between pt-2">
+          <div className="flex gap-2 items-center text-[#1F4386] inter font-[500]">
+          <img className='w-[30px] h-[20px]' src='/logo.png'/>
+
+            June 11th, 2024
+          </div>
+        </div>
       </div>
     </div>
   );
-};
+}
 
-export default BLogCard;
+export default BlogCard;
--- src/pages/blog/BlogList.jsx
+++ src/pages/blog/BlogList.jsx
@@ -1,8 +1,60 @@
 import React from 'react'
+import Navbar from '../../components/Navbar'
+import BlogCard from '../../components/BLogCard'
+import Footer from '../../components/Footer'
 
 const BlogList = () => {
   return (
-    <div>BlogList</div>
+    <div>
+      <Navbar/>
+      <div className='relative md:pt-[200px] lg:pt-24 bg-[#FED676] h-[700px]  lg:h-[900px] md:h-[900px] p-8 flex flex-col justify-start items-center'>
+        <div className='mt-[150px] font-migra font-[800] w-[200px] text-center text-[70px] text-[#FED676] bg-[#1F4386] rounded-[70px]  '>
+          Blog
+        </div>
+        <div>
+        <div className='  flex lg:flex-row md:flex-row flex-col mt-[50px]  '>
+          <img src='/blog.jpg' className='z-10 w-[300px] md:w-[500px] md:h-[300px] lg:w-[650px] lg:h-[400px] object-cover rounded-[20px] md:rounded-[40px] lg:rounded-[40px]' />
+          <div className= 'flex flex-col justify-around items-start bg-[#F6EDDF] pl-4 pb-4 lg:pl-[100px] md:pl-[100px] md:ml-[-90px] lg:ml-[-90px] z-0 w-[300px] lg:w-[400px] md:w-[400px] blog-shadow lg:rounded-[40px] rounded-b-[20px] md:rounded-[40px] mt-[-20px] md:mt-[0px] lg:mt-[0px] '>
+            <div className='text-[25px] pt-[25px] md:pt-0 lg:pt-0 md:text-[30px] lg:text-[30px] text-[#233780] inter font-[600] '>Title</div>
+            <div className='md:text-[20px] lg:text-[20px] text-[15px] text-[#233780] inter font-[400]'>Content</div>
+            <div className='flex justify-center items-center gap-2'>
+              <img className='w-[20px] h-[15px] md:w-[30px] lg:w-[30px] lg:h-[20px] md:h-[20px]' src='/logo.png'/>
+              <div className='md:text-[12px] lg:text-[12px] text-[10px]  text-[#233780] inter font-[600]'>June 11th, 2024</div>
+            </div>
+          </div>
+
+        </div>
+        </div>
+        <img className='absolute w-[80px] md:w-auto lg:w-auto top-[100px] right-[10%]' src='/book.png' />
+        <img className='absolute w-[80px] md:w-auto lg:w-auto  lg:top-[200px] md:top-[200px] left-[10%]' src='/note.png' />
+      </div>
+      <div className='w-full bg-[#F4EBDC]'>
+
+      <div className='pt-10 bg-[#F4EBDC] grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 w-[80%] m-auto'>
+
+      <BlogCard/>
+      <BlogCard/>
+      <BlogCard/>
+      <BlogCard/>
+      <BlogCard/>
+      <BlogCard/>
+      <BlogCard/>
+      <BlogCard/>
+      <BlogCard/>
+      <div>
+
+      </div>
+      <div className='flex gap-4 m-auto mb-12 mt-24'>
+        <img src='/blogarrow.svg' className='w-[8px]' />
+        <div>1,2,3...</div>
+        <img src='/blogarrow.svg' className='w-[8px] rotate-180' />
+
+      </div>
+      </div>
+      </div>
+      <Footer/>
+    </div>
+
   )
 }
 
